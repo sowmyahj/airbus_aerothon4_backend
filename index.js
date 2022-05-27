424170c8665735f43e37a4b27e9b51b3fdceeae3
Aditya <adityasatya100@gmail.com>
2022-05-27 10:51:09 +0530

    port correction

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 var zip = require("express-easy-zip");
-const port = process.env.port || 3002;
+const port = process.env.PORT || 3002;
 
 const router = require("./routes/router");
 
