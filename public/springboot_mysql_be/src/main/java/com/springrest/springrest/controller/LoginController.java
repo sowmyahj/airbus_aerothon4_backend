98936e24ee2dc31b9d7159af6a9e6fbad5be8ac1
Aditya <adityasatya100@gmail.com>
2022-05-28 00:41:27 +0530

    new requests

--- public/springboot_mysql_be/src/main/java/com/springrest/springrest/controller/LoginController.java
+++ public/springboot_mysql_be/src/main/java/com/springrest/springrest/controller/LoginController.java
@@ -1,4 +1,5 @@
 package com.springrest.springrest.controller;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.springrest.entities.Login;
@@ -15,13 +18,13 @@ import com.springrest.springrest.entities.Login;
 import com.springrest.springrest.services.LoginService;
 
 
-@CrossOrigin
+@CrossOrigin("*")
 @RestController
 public class LoginController {
 	@Autowired
 	private LoginService loginService;
 	
-	@GetMapping("/login")
+	@PostMapping("/api/login")
 	public ResponseEntity<Object> getUser(@RequestBody Login login)
 	{	
 		Map<String, String> map = new HashMap<>();
@@ -35,6 +38,19 @@ public class LoginController {
 			return new ResponseEntity<>(map,HttpStatus.BAD_REQUEST);
 			
 		}
+	}
+	@GetMapping("/api/notes")
+	public ResponseEntity<Object> getNotes(){
+		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+		map.put("notes", new ArrayList());
+		return new ResponseEntity<>(map,HttpStatus.OK);
+	}
+	@PostMapping("/api/notes/add")
+	public ResponseEntity<Object> addNotes(@RequestBody Object object){
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("done",false);
+		map.put("note", "NOT SUPPORTED");
+		return new ResponseEntity<>(map,HttpStatus.OK);
 	}
 }
 
